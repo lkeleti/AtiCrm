ff17c7ab5745f58c322eaa4623e07e4c51560616
lkeleti <keleti.laszlo@vipmail.hu>
2023-02-10 10:05:32 +0100

    Configure flyway

---
+++ backend/src/main/resources/db/migration/V2__event.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `events` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `comment` varchar(255) DEFAULT NULL,
+  `event_date` datetime(6) DEFAULT NULL,
+  `partner_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKpartnerid` (`partner_id`),
+  CONSTRAINT `FKpartnerid` FOREIGN KEY (`partner_id`) REFERENCES `partners` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
